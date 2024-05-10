d8b1b12a813f63dddae71804b577fcc09c807076
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-10 09:40:54 +0700

    membuat pop operation

--- stackusinglinkedlist_0136.cpp
+++ stackusinglinkedlist_0136.cpp
@@ -28,4 +28,15 @@ class Stack {
         return value;
 
     }
+
+      void pop() {
+         if (isEmpty()) {
+             cout << "Stack is empty." << endl;
+        }
+         cout << "Popped value: " << top->data << endl;
+        
+   }
+
+    
+
 };
